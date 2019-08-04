75bb8a2810ae220f350717fe5b5ac81877958994
Gil Amran <gil@orbs.com>
2019-08-04 16:14:39 +0300

    Changed the color of the separators

--- src/client/components/sections/HSeparator.tsx
+++ src/client/components/sections/HSeparator.tsx
@@ -11,7 +11,7 @@ import styled from 'styled-components';
 import { theme } from '../base/Theme';
 
 const Root = styled.div`
-  border-bottom: 1px solid ${theme.primaryColor}
+  border-bottom: 1px solid ${theme.darkTextColor}
 `;
 
 export const HSeparator: React.FunctionComponent = () => {
