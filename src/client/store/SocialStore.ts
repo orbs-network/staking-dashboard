b21fc61c9456f2ef06cd8d02e6d7d96520f62baf
Gil Amran <gil@orbs.com>
2019-08-04 16:22:44 +0300

    Added all the socials to read from the store

--- src/client/store/SocialStore.ts
+++ src/client/store/SocialStore.ts
@@ -2,6 +2,8 @@ import { observable, action } from 'mobx';
 
 export class SocialStore {
   @observable public latestTweet: string = '';
+  @observable public latestCommit: string = '';
+  @observable public recentUpdate: string = '';
 
   public async init(): Promise<void> {
     await this.loadStore();
@@ -9,5 +11,7 @@ export class SocialStore {
 
   @action private async loadStore(): Promise<void> {
     this.latestTweet = '#SUMMIT2019 #LIVECRYPTO #KEYWORD #TALKINGABOUTIT #GITHUB #SOCIALIMPACT #GUARDIANS';
+    this.latestCommit = 'orbs-network/orbs-network-go';
+    this.recentUpdate = '25.6.2019 Latest Blog Update';
   }
 }
