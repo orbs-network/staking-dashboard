034c617b8698195622e6510c8d1024d07991d139
Gil Amran <gil@orbs.com>
2019-08-19 16:37:58 +0300

    Added nextVotingTime to the POSStore

--- src/client/store/POSStore.ts
+++ src/client/store/POSStore.ts
@@ -2,6 +2,7 @@ import { observable, action } from 'mobx';
 
 export class POSStore {
   @observable public rewardsDistributed: number = 0;
+  @observable public nextVotingTime: number = 0;
   @observable public topGuardians: string[] = [];
 
   public async init(): Promise<void> {
@@ -10,6 +11,7 @@ export class POSStore {
 
   @action private async loadStore(): Promise<void> {
     this.rewardsDistributed = 68_789;
+    this.nextVotingTime = Date.now() + (15 * 60 * 60) + (45 * 60) + 15;
     this.topGuardians.push('MR.SHOWOFFTOKEN');
     this.topGuardians.push('SLOWMOSHE');
     this.topGuardians.push('DONTTELLMYMAMA');
