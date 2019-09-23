fae30110c3122430f126b422a3d60845bab89caf
Or Lavy <orlavyil@gmail.com>
2019-09-23 16:47:15 +0300

    Removes unused code

--- src/server/routes/pages-router.ts
+++ src/server/routes/pages-router.ts
@@ -4,28 +4,6 @@ import { getManifest } from './manifest-manager';
 import { IStoreInitialData } from '../../shared/IStore';
 import { RealtimeDataProvider } from '../realtime-data/realtimeDataProvider';
 
-function getStoreInitialData(): IStoreInitialData {
-  return {
-    posStore: {
-      blockHeight: 1_234_567,
-      rewardsDistributed: 68_789,
-      nextVotingTime: Date.now() + 15 * 60 * 60 * 1_000 + 45 * 60 * 1_000 + 15 * 1_000,
-      topGuardians: ['MR.SHOWOFFTOKEN', 'SLOWMOSHE', 'DONTTELLMYMAMA'],
-    },
-    socialStore: {
-      latestTweet: '#SUMMIT2019 #LIVECRYPTO #KEYWORD #TALKINGABOUTIT #GITHUB #SOCIALIMPACT #GUARDIANS',
-      latestCommit: 'orbs-network/orbs-network-go',
-      recentUpdate: '25.6.2019 Latest Blog Update',
-    },
-    tokenStore: {
-      orbsInCirculation: 1_700_000_000,
-      tokenPrice: 0.0241,
-      token24HVolume: 40_000_000,
-      totalHolders: 6_000,
-    },
-  };
-}
-
 export function pagesRouter(realTimeDataProvider: RealtimeDataProvider) {
   const router = Router({ mergeParams: true });
 
