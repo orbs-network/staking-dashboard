1c9fde769554905094b8bb2cc93f37e57a118782
Or Lavy <orlavyil@gmail.com>
2019-09-26 14:32:49 +0300

    Adds initialization test for 'SocialStore'

--- src/client/__tests__/store/SocialStore.test.ts
+++ src/client/__tests__/store/SocialStore.test.ts
@@ -1,17 +1,41 @@
 import {instance, mock, verify, when, anyString} from 'ts-mockito';
 import {IGithubService} from '../../services/gitHubService';
 import {SocialStore} from '../../store/SocialStore';
+import {ISocialStoreState} from '../../../shared/IStore';
 
 describe('Social store functionality', () => {
+    let mockedGithubService = mock<IGithubService>();
+
+    // Initialize a new mock
+    beforeEach( () => {
+        mockedGithubService = mock<IGithubService>();
+    });
+
+    // TODO : FUTURE : Add tests for default values.
+
+    it('Constructs properly', () => {
+        const hydratingState: ISocialStoreState = {
+            recentUpdate: 'recentUpdate',
+            latestCommit: 'latestCommit',
+            latestTweet: 'latestTweet',
+        };
+
+        // Get store instance
+        const socialStore: SocialStore = new SocialStore(mockedGithubService, hydratingState);
+
+        // Ensure state is equal to hydrating state
+        expect(socialStore.recentUpdate).toBe(hydratingState.recentUpdate);
+        expect(socialStore.latestCommit).toBe(hydratingState.latestCommit);
+        expect(socialStore.latestTweet).toBe(hydratingState.latestTweet);
+    });
+
     it('Init works properly', async () => {
-        const mockedGithubService = mock<IGithubService>();
 
         // Mock the service
         when(mockedGithubService.getRepoLastCommitGist(anyString(), anyString())).thenResolve({ message: 'lastCommit'});
 
-        const gitHubServiceInstance = instance(mockedGithubService);
-
-        const socialStore: SocialStore = new SocialStore(gitHubServiceInstance);
+        // Get store instance
+        const socialStore = buildWithMocks(mockedGithubService);
 
         // The tested function
         await socialStore.init();
@@ -22,3 +46,11 @@ describe('Social store functionality', () => {
         expect(socialStore.latestCommit).toBe('lastCommit');
     });
 });
+
+function buildWithMocks(mockedGithubService: IGithubService) : SocialStore {
+    const gitHubServiceInstance = instance(mockedGithubService);
+
+    const socialStore: SocialStore = new SocialStore(gitHubServiceInstance);
+
+    return socialStore;
+}
