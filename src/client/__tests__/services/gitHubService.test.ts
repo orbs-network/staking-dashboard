56d7715a3c1e9664119c5ce0d0bb5833e9c30bc2
Or Lavy <orlavyil@gmail.com>
2019-09-26 14:53:53 +0300

    Adds tests for 'gitHubService'

--- src/client/__tests__/services/gitHubService.test.ts
+++ src/client/__tests__/services/gitHubService.test.ts
@@ -1,6 +1,48 @@
+import {anyString, instance, mock, when} from 'ts-mockito';
+import GitHub from 'github-api';
+import {GitHubService, IGithubService} from '../../services/gitHubService';
+import {SocialStore} from '../../store/SocialStore';
 
 describe('Social Data in the app', () => {
-    it('Should work', () => {
+    let mockedGitHubApi: GitHub;
 
+    // Initialize a new mock
+    beforeEach( () => {
+        mockedGitHubApi = mock(GitHub);
+    });
+
+    it('Should extract the last commit message properly', async () => {
+        const expectedLastCommit = 'This is the latest commit';
+
+        // Build the mocked response
+        const mockedRepositoryResponse = {
+            listCommits: async () => {
+                return {
+                 data: [
+                     {
+                         commit: {
+                             message: expectedLastCommit,
+                         }
+                     }
+                 ]
+                };
+            }
+        };
+
+        when(mockedGitHubApi.getRepo(anyString(), anyString())).thenReturn(mockedRepositoryResponse);
+
+        const gitHubService = buildWithMocks(mockedGitHubApi);
+
+        const lastCommitGist = await gitHubService.getRepoLastCommitGist('orbs', 'orbs');
+
+        expect(lastCommitGist).toEqual({ message: expectedLastCommit });
     });
 });
+
+function buildWithMocks(mockedGithubApi: GitHub) : IGithubService {
+    const githubApi = instance(mockedGithubApi);
+
+    const githubService: IGithubService = new GitHubService(githubApi);
+
+    return githubService;
+}
