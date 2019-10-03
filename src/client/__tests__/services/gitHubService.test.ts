724421abc01dfffbb476a46e6df8d32121757a47
Or Lavy <orlavyil@gmail.com>
2019-10-03 11:54:18 +0300

    Adds prettier + fixes

--- src/client/__tests__/services/gitHubService.test.ts
+++ src/client/__tests__/services/gitHubService.test.ts
@@ -1,37 +1,36 @@
-import {anyString, instance, mock, when} from 'ts-mockito';
+import { anyString, instance, mock, when } from 'ts-mockito';
 import GitHub from 'github-api';
-import {GitHubService, IGithubService} from '../../services/gitHubService';
-import {SocialStore} from '../../store/SocialStore';
-import {buildGetRepositoryResponse} from '../testKits/apis/githubApi';
+import { GitHubService, IGithubService } from '../../services/gitHubService';
+import { buildGetRepositoryResponse } from '../testKits/apis/githubApi';
 
 describe('Social Data in the app', () => {
-    let mockedGitHubApi: GitHub;
+  let mockedGitHubApi: GitHub;
 
-    // Initialize a new mock
-    beforeEach( () => {
-        mockedGitHubApi = mock(GitHub);
-    });
+  // Initialize a new mock
+  beforeEach(() => {
+    mockedGitHubApi = mock(GitHub);
+  });
 
-    it('Should extract the last commit message properly', async () => {
-        const expectedLastCommit = 'This is the latest commit';
+  it('Should extract the last commit message properly', async () => {
+    const expectedLastCommit = 'This is the latest commit';
 
-        // Build the mocked response
-        const mockedRepositoryResponse = buildGetRepositoryResponse(expectedLastCommit);
+    // Build the mocked response
+    const mockedRepositoryResponse = buildGetRepositoryResponse(expectedLastCommit);
 
-        when(mockedGitHubApi.getRepo(anyString(), anyString())).thenReturn(mockedRepositoryResponse);
+    when(mockedGitHubApi.getRepo(anyString(), anyString())).thenReturn(mockedRepositoryResponse);
 
-        const gitHubService = buildWithMocks(mockedGitHubApi);
+    const gitHubService = buildWithMocks(mockedGitHubApi);
 
-        const lastCommitGist = await gitHubService.getRepoLastCommitGist('orbs', 'orbs');
+    const lastCommitGist = await gitHubService.getRepoLastCommitGist('orbs', 'orbs');
 
-        expect(lastCommitGist).toEqual({ message: expectedLastCommit });
-    });
+    expect(lastCommitGist).toEqual({ message: expectedLastCommit });
+  });
 });
 
 function buildWithMocks(mockedGithubApi: GitHub): IGithubService {
-    const githubApi = instance(mockedGithubApi);
+  const githubApi = instance(mockedGithubApi);
 
-    const githubService: IGithubService = new GitHubService(githubApi);
+  const githubService: IGithubService = new GitHubService(githubApi);
 
-    return githubService;
+  return githubService;
 }
