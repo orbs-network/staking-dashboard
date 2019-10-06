9bd138fd2c7181981604ed1e1ca95c4e2a027cd3
Or Lavy <orlavyil@gmail.com>
2019-10-06 17:53:49 +0300

    Adds basic PoiPopUp structure

--- src/client/components/globe/PoiPopup.tsx
+++ src/client/components/globe/PoiPopup.tsx
@@ -1,7 +1,8 @@
 import React, { forwardRef } from 'react';
 import styled from 'styled-components';
 
-import { Card } from '@material-ui/core';
+import { Avatar, Button, Card, CardContent, CardHeader, Divider } from '@material-ui/core';
+import { Link } from '@material-ui/icons';
 
 interface IProps {
   name: string;
@@ -34,12 +35,38 @@ const StyledHeader = styled.h1`
   text-align: center;
 `;
 
+const PopUpCard = styled(Card)<{ top: number; left: number }>`
+  position: absolute;
+  // height: ${height}px;
+  // width: ${width}px;
+  width: 20%;
+
+  // Positioning
+  left: ${props => props.left}px;
+  top: ${props => props.top}px; // TODO : ORL : Find out how to raise the card by its own height
+`;
+
 export const PoiPopup = forwardRef<Ref, IProps>((props, ref) => {
   const { top, left } = props;
 
   return (
-    <StyledDiv top={top} left={left} ref={ref}>
-      <StyledHeader>Cool data about node - {props.name}</StyledHeader>
-    </StyledDiv>
+    <PopUpCard top={top} left={left} ref={ref}>
+      <CardHeader
+        avatar={<Avatar alt={'A'} src={'https://www.orbs.com/wp-content/uploads/2019/02/Andrey-Dulkin-Orbs.jpg'} />}
+        title={'Andrey Tarantinov'}
+        subheader={'South Korea'}
+        action={
+          <Button variant='outlined' size='small' startIcon={<Link />}>
+            Guardian
+          </Button>
+        }
+      />
+      <CardContent>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        <Divider />
+        Rank #9 Total stake: 23,000 Orbs Active since: 2018
+      </CardContent>
+      {/*<StyledHeader>Cool data about node - {props.name}</StyledHeader>*/}
+    </PopUpCard>
   );
 });
