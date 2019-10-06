80d45bc5c66eaa864b7f7def9a51be957e660e20
Or Lavy <orlavyil@gmail.com>
2019-10-06 16:09:25 +0300

    Changes PoiPopup to used  styled components

--- src/client/components/globe/PoiPopup.tsx
+++ src/client/components/globe/PoiPopup.tsx
@@ -1,4 +1,7 @@
 import React, { forwardRef } from 'react';
+import styled from 'styled-components';
+
+import { Card } from '@material-ui/core';
 
 interface IProps {
   name: string;
@@ -11,26 +14,32 @@ type Ref = HTMLDivElement;
 const height = 100;
 const width = 200;
 
-export const PoiPopup = forwardRef<Ref, IProps>((props, ref) => {
-  return (
-    <div
-      style={{
-        position: 'absolute',
-        borderWidth: 1,
-        borderColor: 'red',
-        borderStyle: 'solid',
-        height,
-        width,
-        alignContent: 'center',
-        alignItems: 'center',
-        opacity: 0,
+const StyledDiv = styled.div<{ top: number; left: number }>`
+  position: absolute;
+  border: 1px solid red;
 
-        left: props.left,
-        top: props.top - height - 5,
-      }}
-      ref={ref}
-    >
-      <h1 style={{ color: 'red', textAlign: 'center' }}>Cool data about node - {props.name}</h1>
-    </div>
+  height: ${height}px;
+  width: ${width}px;
+
+  align-content: 'center';
+  align-items: 'center';
+
+  // Positioning
+  left: ${props => props.left}px;
+  top: ${props => props.top - height - 5}px;
+`;
+
+const StyledHeader = styled.h1`
+  color: blue;
+  text-align: center;
+`;
+
+export const PoiPopup = forwardRef<Ref, IProps>((props, ref) => {
+  const { top, left } = props;
+
+  return (
+    <StyledDiv top={top} left={left} ref={ref}>
+      <StyledHeader>Cool data about node - {props.name}</StyledHeader>
+    </StyledDiv>
   );
 });
