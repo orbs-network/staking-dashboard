e1c782a17c8333777622b61dfd545ea1a6c8c116
Or Lavy <orlavyil@gmail.com>
2019-10-27 17:49:10 +0200

    Removes data faking for token store

--- src/client/store/TokenStore.ts
+++ src/client/store/TokenStore.ts
@@ -24,39 +24,7 @@ export class TokenStore implements ITokenStore {
     this.tokenPrice = initialData.tokenPrice;
     this.token24HVolume = initialData.token24HVolume;
     this.totalHolders = initialData.totalHolders;
-
-    this.startFakingChanges();
   }
 
   public async init(): Promise<void> {}
-
-  @action
-  private fakeIncreaseCirculation() {
-    // 50 <-> 150
-    const totalOrbsChange = Math.random() * 100 + 50;
-
-    this.orbsInCirculation += totalOrbsChange;
-  }
-
-  @action
-  private fakeUpdateTokenPrice() {
-    // 0.5 <-> 1.5
-    const percentageChange = Math.random() + 0.5;
-
-    this.tokenPrice *= percentageChange;
-  }
-
-  @action
-  private fakeUpdateTotalHolders() {
-    // -50 <-> 50
-    const totalHoldersChange = Math.round(Math.random() * 100 + -50);
-
-    this.totalHolders += totalHoldersChange;
-  }
-
-  private startFakingChanges() {
-    setInterval(this.fakeIncreaseCirculation.bind(this), 5_000);
-    setInterval(this.fakeUpdateTokenPrice.bind(this), 10_000);
-    setInterval(this.fakeUpdateTotalHolders.bind(this), 10_000);
-  }
 }
