407840105016e87f975d15ded0c7f345dcd91519
Or Lavy <orlavyil@gmail.com>
2019-10-30 13:23:48 +0200

    Makes the Poi popup responsive

--- src/client/components/globe/poiCard/PoiPopup.tsx
+++ src/client/components/globe/poiCard/PoiPopup.tsx
@@ -1,9 +1,8 @@
 import React, { forwardRef } from 'react';
 import styled from 'styled-components';
 
-import { POI_CARD_WIDTH_IN_PX, PoiCard } from './PoiCard';
-import { theme } from '../../base/Theme';
 import Color from 'color';
+import { PoiCardSelf } from './PoiCardSelf';
 
 interface IProps {
   location: string;
@@ -11,18 +10,19 @@ interface IProps {
 
 type Ref = HTMLDivElement;
 
-const POP_UP_TOP_MARGIN = 20;
-const POP_UP_OUTER_ARROW_WIDTH = POP_UP_TOP_MARGIN;
-const POP_UP_INNER_ARROW_WIDTH = POP_UP_OUTER_ARROW_WIDTH - 3;
+const POP_UP_TOP_MARGIN_EM = 1;
+const POP_UP_OUTER_ARROW_WIDTH_EM = POP_UP_TOP_MARGIN_EM;
+const POP_UP_INNER_ARROW_WIDTH_EM = POP_UP_OUTER_ARROW_WIDTH_EM * 0.8;
 
-const StyledPopUpDiv = styled('div')({
+const StyledPopUpDiv = styled('div')(({ theme }) => ({
   // These properties ensures that we display the pop up in just a bit below (marginTop)
   // the it's container's center
   position: 'absolute',
   left: '50%',
-  marginLeft: -(POI_CARD_WIDTH_IN_PX / 2),
+  // marginLeft: -(POI_CARD_WIDTH_IN_PX / 2),
+  marginLeft: '-13em',
   top: '50%',
-  marginTop: 20,
+  marginTop: `${POP_UP_TOP_MARGIN_EM}em`,
 
   // We keep the popup background clear to allow the card's style to take effect
   backgroundColor: 'rgba(0, 0, 0, 0)',
@@ -35,7 +35,7 @@ const StyledPopUpDiv = styled('div')({
 
   // *****************************************
   // This part handles the 'tooltip arrow'
-  // DEV_NOTE : Taken from http://www.cssarrowplease.com/
+  // DEV_NOTE : Inspiration taken from http://www.cssarrowplease.com/
   // *****************************************
 
   // Rules for both outer and inner triangles
@@ -55,24 +55,21 @@ const StyledPopUpDiv = styled('div')({
 
   // The inner triangle
   '&::after': {
-    // borderRadius: 1,
-
     // Handle visibility and color (transparent besides the bottom)
     borderStyle: 'solid',
     borderColor: 'rgba(0, 0, 0, 0)',
 
     // Should match the popup border **background** color (with the same alpha value)
-    // DEV_NOTE : we reduce the alpha value in order to compensate about the fact that the
-    //  inner triangle is sitting upon the outer one, which makes the final result more opaque.
+    // DEV_NOTE : We reduce the alpha value in order to compensate about the fact that the
+    //            inner triangle is sitting upon the outer one, which makes the final result more opaque.
     borderBottomColor: Color(theme.poiCardBackgroundColor)
       .alpha(theme.poiCardBackgroundAlpha * 0.8)
       .rgb()
       .toString(),
-    // borderBottomColor: 'rgba(20, 20, 20, 0.6)',
 
     // Size and margin fix to ensure proper centering.
-    borderWidth: POP_UP_INNER_ARROW_WIDTH,
-    marginLeft: -POP_UP_INNER_ARROW_WIDTH,
+    borderWidth: `${POP_UP_INNER_ARROW_WIDTH_EM}em`,
+    marginLeft: `${calculatePopUpArrowLeftMargin(theme.cardTheme.paddingInEm, POP_UP_INNER_ARROW_WIDTH_EM)}em`,
   },
 
   // The outer triangle
@@ -89,10 +86,10 @@ const StyledPopUpDiv = styled('div')({
       .toString(),
 
     // Size and margin fix to ensure proper centering.
-    borderWidth: POP_UP_OUTER_ARROW_WIDTH,
-    marginLeft: -POP_UP_OUTER_ARROW_WIDTH,
+    borderWidth: `${POP_UP_OUTER_ARROW_WIDTH_EM}em`,
+    marginLeft: `${calculatePopUpArrowLeftMargin(theme.cardTheme.paddingInEm, POP_UP_OUTER_ARROW_WIDTH_EM)}em`,
   },
-});
+}));
 
 export const PoiPopup = React.memo(
   forwardRef<Ref, IProps>((props, ref) => {
@@ -100,8 +97,17 @@ export const PoiPopup = React.memo(
 
     return (
       <StyledPopUpDiv ref={ref}>
-        <PoiCard location={location} />
+        <PoiCardSelf location={location} />
       </StyledPopUpDiv>
     );
   }),
 );
+
+/**
+ * Calculating the appropriate margin left for the tooltip arrows.
+ */
+function calculatePopUpArrowLeftMargin(innerElementPadding: number, arrowWidth: number): number {
+  const halfInnerElementPadding = innerElementPadding / 2;
+
+  return -(halfInnerElementPadding + arrowWidth);
+}
